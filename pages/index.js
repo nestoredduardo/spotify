681a08f5f4d43485c384e8053c5c5d7ecc820636
Nestor Mamani <nmamanipantoja@gmail.com>
2021-12-06 09:30:42 -0500

    Sidebar Mobile

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className="min-h-screen relative bg-background">
         <Sidebar />
       </main>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#121212',
+        aside: '#282828',
+        unfocus: '#e6e6e6',
+        focus: '#fefefe',
+      },
+    },
   },
   variants: {
     extend: {},
