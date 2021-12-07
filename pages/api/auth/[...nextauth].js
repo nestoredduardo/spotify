8a42beef28e95b8773bec612a931f1be90f1f946
Nestor Mamani <nmamanipantoja@gmail.com>
2021-12-07 08:46:00 -0500

    Spotify utils

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
 import SpotifyProvider from 'next-auth/providers/spotify'
+import { LOGIN_URL } from '@utils/spotify'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -7,7 +8,9 @@ export default NextAuth({
     SpotifyProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+      authorization: LOGIN_URL,
     }),
     // ...add more providers here
   ],
+  secret: process.env.JWT_SECRET,
 })
