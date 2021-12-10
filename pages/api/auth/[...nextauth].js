b21b6e23c3f599d7dc68a902f23d0bff4f3635d4
Nestor Mamani <nmamanipantoja@gmail.com>
2021-12-10 10:36:06 -0500

    Refresh Access Token

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,7 +4,17 @@ import spotifyApi, { LOGIN_URL } from '@utils/spotify'
 
 const refreshAccessToken = async (token) => {
   try {
-    spotifyApi.setAccessToken
+    spotifyApi.setAccessToken(token.accessToken)
+    spotifyApi.setRefreshToken(token.refreshToken)
+
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken()
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now + refreshedToken.expires_in * 1000,
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+    }
   } catch (error) {
     return {
       ...token,
